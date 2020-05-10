9c04d4dd5664702be97e653803979dd59176e308
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-05-10 12:50:02 +0200

    Add documentation

--- web/api/api.py
+++ web/api/api.py
@@ -29,6 +29,15 @@ def conflict(e):
 # authorized
 @api.route('/person/<string:username>', methods=['GET'])
 def get_person(username: str):
+    """
+    Get person data for logged in user
+
+    Args:
+        username: logged in user
+
+    Returns: Person json string
+
+    """
     persons = Person.query.filter_by(username=username).first()
     if persons is None:
         return abort(404, description='Person not found')
@@ -40,6 +49,12 @@ def get_person(username: str):
 # add/create user
 @api.route('/person', methods=['POST'])
 def add_person():
+    """
+    Add a new person to db, or error if username already taken
+
+    Returns: Json string of added user
+
+    """
     schema = PersonSchema(exclude=['id'])
     try:
         person = schema.loads(request.get_json())
@@ -55,6 +70,14 @@ def add_person():
 # authorized
 @api.route('/person/<string:username>/booking', methods=['POST'])
 def add_booking(username: str):
+    """
+    Add a new booking for this user
+    Args:
+        username: logged in user
+
+    Returns: Json string of booking, or error
+
+    """
     # bookings can't already have an id
     schema = BookingSchema(exclude=['id'])
     try:
@@ -83,6 +106,14 @@ def add_booking(username: str):
 # authorized!!
 @api.route('/person/<string:username>/booking', methods=['GET'])
 def get_bookings(username: str):
+    """
+    Get all bookings for this user
+    Args:
+        username: logged in user
+
+    Returns: All bookings for user, or 404
+
+    """
     schema = BookingSchema(many=True)
     person = Person.query.filter_by(username=username).first()
     if person is None:
@@ -95,6 +126,16 @@ def get_bookings(username: str):
 # authorized!!
 @api.route('/person/<string:username>/booking/<int:id>', methods=['GET'])
 def get_booking(username: str, id: int):
+    """
+    Get booking for given user and booking id
+
+    Args:
+        username: logged in user
+        id: booking id
+
+    Returns: One booking if exists, else 404
+
+    """
     schema = BookingSchema()
     booking = Booking.query.get(id)
     if booking is None or booking.person.username != username:
@@ -108,8 +149,11 @@ def get_cars():
     """
     Get cars based of query GET arguments/form POST arguments (supports both).
     All arguments must be in the Cars class otherwise a 404 is returned
+    Example:
+        GET /api/car?car_colour=1&car_manufacturer=2
+        (note the id is used, not the name)
 
-    Returns: Json string of list of Cars with given arguments
+    Returns: Json list string of Cars with given arguments
     """
     filters = request.values.to_dict()
     if len(filters) > 0:
@@ -125,6 +169,12 @@ def get_cars():
 
 @api.route('/car-manufacturer', methods=['GET'])
 def get_manufacturers():
+    """
+    Get all manufacturers
+
+    Returns: All manufacturers as json list string
+
+    """
     manufacturers = CarManufacturer.query.all()
     result = CarManufacturerSchema(many=True).dumps(manufacturers)
     return jsonify(result), 200
@@ -132,6 +182,12 @@ def get_manufacturers():
 
 @api.route('/car-type', methods=['GET'])
 def get_car_types():
+    """
+    Get all car types
+
+    Returns: All car types as json list string
+
+    """
     types = CarType.query.all()
     result = CarTypeSchema(many=True).dumps(types)
     return jsonify(result), 200
@@ -139,6 +195,12 @@ def get_car_types():
 
 @api.route('/car-colour', methods=['GET'])
 def get_car_colours():
+    """
+    Get all car colours.
+
+    Returns: All car colours as json list string
+
+    """
     colours = CarColour.query.all()
     result = CarColourSchema(many=True).dumps(colours)
     return jsonify(result), 200
@@ -146,6 +208,12 @@ def get_car_colours():
 
 @api.route('/booking-status', methods=['GET'])
 def get_booking_statuses():
+    """
+    Get all booking statuses
+
+    Returns: All booking statuses as json list string
+
+    """
     statuses = BookingStatus.query.all()
     result = BookingStatusSchema(many=True).dumps(statuses)
     return jsonify(result), 200
@@ -155,6 +223,12 @@ def get_booking_statuses():
 # adding this data is allows to do manually
 @api.route('/person_type', methods=['POST'])
 def add_person_type():
+    """
+    Add person type. For admins.
+
+    Returns: Added person type as json string.
+
+    """
     schema = PersonTypeSchema()
     person_type = schema.loads(request.get_json())
     db.session.add(person_type)
@@ -164,6 +238,12 @@ def add_person_type():
 
 @api.route('/person', methods=['GET'])
 def get_persons():
+    """
+    Get all persons. For admins.
+
+    Returns: All persons as json list string.
+
+    """
     persons = Person.query.all()
     result = PersonSchema(many=True).dump(persons)
     return jsonify(result), 200
