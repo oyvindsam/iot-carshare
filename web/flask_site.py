ec46e3f5a622bc67a39a0556494766aff41bac81
Aditya Raj <rajadi2797@gmail.com>
2020-05-23 18:21:30 +1000

    Added google calendar integration for booking cars

--- web/flask_site.py
+++ web/flask_site.py
@@ -4,10 +4,18 @@ from flask_sqlalchemy import SQLAlchemy
 from urllib.parse import unquote, urlparse
 from flask_marshmallow import Marshmallow
 from flask_wtf import FlaskForm
+from googleapiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+from datetime import datetime
 import os, requests, json
 import urllib
 import ast
-
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
 
 site = Blueprint("site", __name__)
@@ -103,19 +111,94 @@ def bookcar():
     #     data = response.text
     #   return render_template("bookingConfirm.html", resp = data)
 
-#method after a car has been selected to be booked
+# method after a car has been selected to be booked
 @site.route("/time/<carinfo>", methods=["GET", "POST"])
 def time(carinfo):
      if request.method == 'POST':
          decoded_query = urllib.parse.unquote(carinfo)
          decode1=ast.literal_eval(decoded_query)
-         print(decoded_query)
-         print(type(decoded_query))
-         print(decode1)
-         print(type(decode1))
-         
          return render_template("time.html", info=decode1)
-    
+
+# method after to add booking to google calendar
+@site.route("/timeBook", methods=["GET", "POST"])
+def timeBook():
+    carid = request.form['car_id']
+    make = request.form['make']
+    cartype = request.form['type']
+    carreg = request.form['reg']
+    rate = request.form['rate']
+    longitude = request.form['longitude']
+    latitude = request.form['latitude']
+    location= 'http://www.google.com/maps/place/'+latitude+','+longitude
+    startDateTime = request.form['bookingstarttime']
+    endDateTime = request.form['bookingendtime']
+    startDateTime = startDateTime + ':00+10:00'
+    endDateTime = endDateTime + ':00+10:00'
+    print(startDateTime)
+    print(endDateTime)
+    print(carid)
+    SCOPES = ["https://www.googleapis.com/auth/calendar"]
+    store = file.Storage('token.json')
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    event = {
+                'summary': 'Novo Car share booking',
+                'location': location,
+                'description': 'Your car booking with Novoshare with the following car details of your car: '+ make +' with registration: '+ carreg+ ' and type is: '+cartype + ' and the Hourly Rate is: ' + rate,
+                'start': {
+                    'dateTime': startDateTime,
+                    'timeZone': "Australia/Melbourne",
+                },
+                'end': {
+                    'dateTime': endDateTime,
+                    'timeZone': "Australia/Melbourne",
+                },
+                'recurrence': [
+                    'RRULE:FREQ=DAILY;COUNT=2'
+                ],
+                'attendees': [
+                    {'email': 'lpage@example.com'},
+                    {'email': 'sbrin@example.com'},
+                ],
+                'reminders': {
+                    'useDefault': False,
+                    'overrides': [
+                    {'method': 'email', 'minutes': 24 * 60},
+                    {'method': 'popup', 'minutes': 10},
+                    ],
+                },
+                }
+    event = service.events().insert(calendarId='primary', body=event).execute()
+    google_event_link = event.get('htmlLink')
+    print (google_event_link)
+    return render_template("confirmation.html", invite=google_event_link) 
+
+
+    # This next section's code will be here
+
+# @site.route("/mainbooking")
+# def mainbooking():
+
+
 #view previous bookings
 @site.route("/history")
 def hist():
