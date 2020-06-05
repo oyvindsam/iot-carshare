f029e0a11df7ab502c214a75172afe96b37f3d99
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-06-05 12:39:10 +0200

    Add decorator for roles

--- web/api/auth.py
+++ web/api/auth.py
@@ -1,29 +1,60 @@
-from flask import request, abort
-from flask_jwt_extended import create_access_token, create_refresh_token
+from functools import wraps
+
+from flask import request, abort, jsonify
+from flask_jwt_extended import jwt_required, create_access_token, \
+    verify_jwt_in_request, get_jwt_claims
 from marshmallow import ValidationError
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from api import auth, api_blueprint
-from api.models import Person, PersonSchema, PersonType, db
+from api import api_blueprint, jwt
+from api.models import Person, PersonSchema, db
+
+# Read up on how to create token with user id and roles
+# https://flask-jwt-extended.readthedocs.io/en/stable/tokens_from_complex_object/
+
+@jwt.user_claims_loader
+def add_claims_to_access_token(person: Person):
+    """
+    Returns user role (person type)
+
+    Args:
+        person (Person): person who generates token
+
+    Returns: roles for this user
+
+    """
+    return person.person_type
+
+@jwt.user_identity_loader
+def user_identity_lookup(person: Person):
+    """
+    Find identity from user object
+    Args:
+        person (Person): person who generates token
+
+    Returns: user id
+
+    """
+    return person.id
 
 
-@api_blueprint.route('register', methods=['POST'])
-def register_user():
-    schema = PersonSchema(exclude=['id', 'person_type'])
-    try:
-        person = schema.loads(request.get_json())
-    except ValidationError:
-        return abort(400, description='Invalid person data')
-    if Person.query.filter_by(username=person.username).first() is not None:
-        return abort(409, description='User exists')
+@jwt.user_loader_callback_loader
+def user_loader_callback(identity):
+    return Person.query.get(identity)
 
-    # the passed password is not hashed, hash it.
-    person.password_hashed = generate_password_hash(person.password_hashed)
 
-    db.session.add(person)
-    db.session.commit()
-
-    return {'User created!'}, 201
+def role_required(roles):
+    def check_role(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            verify_jwt_in_request()
+            claims = get_jwt_claims()
+            if claims['roles'] not in roles:
+                return jsonify(msg='Does not have permission to access this endpoint!'), 403
+            else:
+                return fn(*args, **kwargs)
+        return wrapper
+    return check_role
 
 
 @api_blueprint.route('login', methods=['POST'])
@@ -38,8 +69,6 @@ def login_user():
     if person is None or not check_password_hash(person.password_hashed, password):
         return abort(403, 'Invalid username or password')
 
-    access_token = create_access_token(identity=person.id, expires_delta=False)
-    return {
-        'message': 'Logged in as {}'.format(person.username),
-        'access_token': access_token,
-    }
+    access_token = create_access_token(identity=person, expires_delta=False)
+    return jsonify(access_token=access_token), 200
+
