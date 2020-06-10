c17b888e6ae38ed6cb05f1f995c85cf6bb39d635
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-06-10 19:45:52 +0200

    show booking detail

--- web/site_web/site_admin.py
+++ web/site_web/site_admin.py
@@ -1,7 +1,7 @@
 import json
 
 import requests
-from flask import render_template, session, redirect
+from flask import render_template, session, redirect, request
 
 from site_web import site_blueprint
 from site_web.flask_site import api_address
@@ -18,8 +18,12 @@ def bookings():
     return render_template('admin/booking-history.html', bookings_data=bookings_data.json())
 
 
+
 @site_blueprint.route('/admin/booking/<int:id>')
 def booking_detail(id):
-    booking_data = requests.get(f"{api_address}/api/booking/{id}",  headers=session['auth'])
-    booking = json.loads(booking_data.json())
-    return render_template('admin/booking-detail.html', booking=booking)
+    if request.method == 'GET':
+        booking_data = requests.get(f"{api_address}/api/booking/{id}",  headers=session['auth'])
+        booking = json.loads(booking_data.json())
+        return render_template('admin/booking-detail.html', booking=booking)
+    elif request.method == 'POST':
+        data = request.get_json()
