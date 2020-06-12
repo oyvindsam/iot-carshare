d9b4e677b21f473155b7a2e5428262c3b522f972
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-06-12 12:41:23 +0200

    Add init car loading code

--- web/site_web/site_engineer.py
+++ web/site_web/site_engineer.py
@@ -1,10 +1,19 @@
-from flask import render_template
+import requests
+from flask import render_template, session
 
 from site_web import site_blueprint
+from site_web.flask_site import api_address
 from site_web.site_login import require_type
 
 
 @site_blueprint.route('/engineer')
 @require_type(['ENGINEER', 'ADMIN'])
 def engineer():
-    return render_template('engineer.html')
+    """
+    Load cars from api and pass to car list view
+    Returns: view
+
+    """
+    car_data = requests.get(f"{api_address}/api/engineer/car",
+                            headers=session['auth'])
+    return render_template('engineer.html', car_data=car_data.json())
