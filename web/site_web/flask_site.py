5ae3c1fde78ef741fcdc6365a9dd4a346a240faa
Øyvind Samuelsen <oyvinsam@stud.ntnu.no>
2020-06-13 08:23:20 +0200

    'fix' calendar deletion

--- web/site_web/flask_site.py
+++ web/site_web/flask_site.py
@@ -258,38 +258,38 @@ def cancelbook():
     of the user to do a DELETE request which updates the bookings with the booking which was cancelled
     """
 
-    calID = request.form['googleid']
     bookingID = request.form['bookingId']
-    usrName = request.form['username']
-    decoder = base64.b64decode(calID+'=').decode('utf-8')
-    decoder = decoder.split()
-    calID = decoder[0]
-    str(bookingID)
-    SCOPES = ["https://www.googleapis.com/auth/calendar"]
-    store = file.Storage('token.json')
-    creds = None
-
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                '../credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-
-    service = build('calendar', 'v3', credentials=creds)
-    response = service.events().delete(calendarId='primary', eventId = calID).execute()
+    # calID = request.form['googleid']
+    # usrName = request.form['username']
+    # decoder = base64.b64decode(calID+'=').decode('utf-8')
+    # decoder = decoder.split()
+    # calID = decoder[0]
+    # str(bookingID)
+    # SCOPES = ["https://www.googleapis.com/auth/calendar"]
+    # store = file.Storage('token.json')
+    # creds = None
+    #
+    # # The file token.pickle stores the user's access and refresh tokens, and is
+    # # created automatically when the authorization flow completes for the first
+    # # time.
+    #
+    # if os.path.exists('token.pickle'):
+    #     with open('token.pickle', 'rb') as token:
+    #         creds = pickle.load(token)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             '../credentials.json', SCOPES)
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open('token.pickle', 'wb') as token:
+    #         pickle.dump(creds, token)
+    #
+    # service = build('calendar', 'v3', credentials=creds)
+    # response = service.events().delete(calendarId='primary', eventId = calID).execute()
     #print(response)
     username = session['person']['username']
     url = f"{api_address}/api/person/{username}/booking/{bookingID}"
